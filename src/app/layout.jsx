a0f6071aa6e0c8b23821da90e4ba256281b78368
Félix Orain <felix.orain@gmail.com>
2025-08-06 19:27:00 +0200

    Refactor layout and styles: Added base text class to body and removed font-size from globals.css

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body>
+      <body className="text-base">
         <Providers>{children}</Providers>
       </body>
     </html>
